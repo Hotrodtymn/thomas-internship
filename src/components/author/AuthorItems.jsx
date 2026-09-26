da56952e66473e22636f3ab2114b32af71613573
Thomas Babb <tomleebabb@gmail.com>
2026-09-26 14:36:12 -0400

    test

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,13 +1,7 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
 
-const AuthorItems = ({
-  items = [],
-  loading,
-  authorImage,
-  authorId,
-}) => {
+const AuthorItems = ({ items = [], loading, authorImage, authorId }) => {
   return (
     <div className="de_tab_content">
       <div className="tab-1">
@@ -65,18 +59,8 @@ const AuthorItems = ({
                 <div className="nft__item">
                   {/* Author */}
                   <div className="author_list_pp">
-                    <Link
-                      to={
-                        authorId
-                          ? `/author/${authorId}`
-                          : "/author"
-                      }
-                    >
-                      <img
-                        className="lazy"
-                        src={authorImage}
-                        alt="Author"
-                      />
+                    <Link to={authorId ? `/author/${authorId}` : "/author"}>
+                      <img className="lazy" src={authorImage} alt="Author" />
 
                       <i className="fa fa-check"></i>
                     </Link>
@@ -93,7 +77,7 @@ const AuthorItems = ({
 
                           {/* Facebook */}
                           <a
-                            href="https://www.facebook.com/"
+                            href="#"
                             target="_blank"
                             rel="noopener noreferrer"
                             aria-label="Share on Facebook"
@@ -103,7 +87,7 @@ const AuthorItems = ({
 
                           {/* X / Twitter */}
                           <a
-                            href="https://x.com/"
+                            href="#"
                             target="_blank"
                             rel="noopener noreferrer"
                             aria-label="Share on X"
@@ -112,19 +96,14 @@ const AuthorItems = ({
                           </a>
 
                           {/* Email */}
-                          <a
-                            href="mailto:?subject=Check out this NFT&body=Check out this NFT!"
-                            aria-label="Share by email"
-                          >
+                          <a href="#" aria-label="Share by email">
                             <i className="fa fa-envelope fa-lg"></i>
                           </a>
                         </div>
                       </div>
                     </div>
 
-                    <Link
-                      to={`/item-details/${item.nftId}`}
-                    >
+                    <Link to={`/item-details/${item.nftId}`}>
                       <img
                         src={item.nftImage}
                         className="lazy nft__item_preview"
@@ -135,15 +114,11 @@ const AuthorItems = ({
 
                   {/* NFT Information */}
                   <div className="nft__item_info">
-                    <Link
-                      to={`/item-details/${item.nftId}`}
-                    >
+                    <Link to={`/item-details/${item.nftId}`}>
                       <h4>{item.title}</h4>
                     </Link>
 
-                    <div className="nft__item_price">
-                      {item.price} ETH
-                    </div>
+                    <div className="nft__item_price">{item.price} ETH</div>
 
                     <div className="nft__item_like">
                       <i className="fa fa-heart"></i>
